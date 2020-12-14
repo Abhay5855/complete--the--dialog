a225af4253a0b9339ceac3e7909be70078c87fca
Abhay Patil <ap3919529@gmail.com>
2020-12-14 13:09:57 +0000

    feat: add play function with parameter question and answer

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ score = 0;
 var gameAnswer = readlineSync.question(chalk.red("Lets play a game!"))
 
 console.log(chalk.yellow("Chalo " + gameAnswer + " dialog complete karte hain."))
+
+function play(question,answer){
+  
+}
